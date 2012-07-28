de3119eaf1d91194c824df602f1ee5c4e8de6326
Kevin William Henwood <ski@cpan.org>
2012-07-28 21:07:50 +0000

    Import of SKI/Task-Mechanistic-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Mechanistic
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SKI/Task-Mechanistic-0.01.tar.gz
    gitpan-cpan-author:       SKI
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Mechanistic.pm
@@ -0,0 +1,41 @@
+package Task::Mechanistic;
+
+use 5.005;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+
+=head1 NAME
+
+Task::Mechanistic - Perl extension to install WWW::Mechanize
+
+=head1 SYNOPSIS
+
+  cpan Task::Mechanistic
+
+=head1 DESCRIPTION
+
+Installs WWW::Mechanize and its dependencies
+
+=head2 EXPORT
+
+  None by default.
+
+=head1 AUTHOR
+
+Kevin W. Henwood, <Khen1950fx@aol.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Kevin W. Henwood
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.005 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ t/Task-Mechanistic.t
@@ -0,0 +1,18 @@
+# Before `make install' is performed this script should be runnable with
+# `make test'. After `make install' it should work as `perl Task-Mechanistic.t'
+
+#########################
+
+
+use strict;
+use warnings;
+
+use Test;
+BEGIN { plan tests => 1 };
+use Task::Mechanistic;
+ok(1); 
+
+#########################
+
+
+
